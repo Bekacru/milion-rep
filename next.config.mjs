3d37a0dd175e93540703416728a47a812afe0e63
bekacru <bekacru@gmail.com>
2023-08-09 08:58:27 +0300

    init

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+import million from "million/compiler"
+const nextConfig = {}
+
+export default million.next(nextConfig, {
+    auto: {
+        rsc: true
+    }
+})
